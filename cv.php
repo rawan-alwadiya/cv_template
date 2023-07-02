9b3ede1679be6e6e7da9250b07d7da6e46ba05c4
hp <r.wadia21@gmail.com>
2023-07-03 00:47:41 +0100

    cv

---
+++ cv.php
@@ -0,0 +1,220 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <script src="https://kit.fontawesome.com/0b90469445.js" crossorigin="anonymous"></script>
+
+</head>
+<body>
+    <div class="cv_wrapper">
+    <div class="cv_left">
+        <div class="cv_image">
+            <img src="./images/cv_image.jpg" alt="cv_image">
+        </div>
+        <div class="cv_bottom">
+            <div class="cv_item cv_namerule">
+                <div class="name">Mohammed Alwadiya</div>
+                <div class="role">Software Engineer</div>
+            </div>
+            <div class="cv_item cv_profile">
+                <div class="cv_title">Profile</div>
+                <div class="cv_info">Lorem ipsum dolor, illum consequuntur, accusamus asperiores dolorum ad expedita, nisi voluptate unde voluptatem explicabo eius. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere odio nisi consectetur error, et ipsum perspiciatis soluta a, quaerat unde, veniam fuga. Explicabo qui voluptatum perspiciatis numquam delectus provident omnis? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus adipisci et consequuntur, modi provident laboriosam nulla nam quo commodi eos incidunt aliquid blanditiis, aut dolorum delectus ad tempora aperiam vero? Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, minima vitae qui exercitationem fuga similique, esse totam ab cum eveniet laboriosam hic alias ratione sequi. Soluta quasi expedita ea harum.</div>
+            </div>
+            <div class="cv_item cv_address">
+                <div class="cv_title">Address</div>
+                <div class="cv_info">
+                    18th home, <br>
+                    Hamdan street, <br>
+                    Abu Dhabi, United Arab Emirates
+                </div>
+            </div>
+            <div class="cv_item cv_contact">
+            <div class="cv_title">Contact</div>
+                <div class="cv_info">
+                    <div class="cv_subtitle">Phone</div>
+                    <div class="cv_subinfo">+071 56 123 4567</div>
+                </div>
+                <div class="cv_info">
+                    <div class="cv_subtitle">Email</div>
+                    <div class="cv_subinfo">r.wadia21@gmail.com</div>
+                </div>
+            </div>
+            <div class="cv_item cv_skills">
+            <div class="cv_title">Skills</div>
+            <div class="cv_info">
+                    <div class="skills_list">
+                        <div class="skills_left">HTML</div>
+                        <div class="skills_bar">
+                            <p>
+                                <span style="width: 90%"></span>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="skills_list">
+                        <div class="skills_left">CSS</div>
+                        <div class="skills_bar">
+                            <p>
+                                <span style="width: 80%"></span>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="skills_list">
+                        <div class="skills_left">JAVASCRIPT</div>
+                        <div class="skills_bar">
+                            <p>
+                                <span style="width: 50%"></span>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="skills_list">
+                        <div class="skills_left">FLUTTER</div>
+                        <div class="skills_bar">
+                            <p>
+                                <span style="width: 95%"></span>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="skills_list">
+                        <div class="skills_left">REACT JS</div>
+                        <div class="skills_bar">
+                            <p>
+                                <span style="width: 75%"></span>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="skills_list">
+                        <div class="skills_left">NODE JS</div>
+                        <div class="skills_bar">
+                            <p>
+                                <span style="width: 90%"></span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="cv_right">
+    <div class="cv_item cv_namerule">
+                <div class="name">Mohammed Alwadiya</div>
+                <div class="role">Software Engineer</div>
+            </div>
+            <div class="cv_item cv_education">
+                <div class="cv_title">Education</div>
+                <div class="cv_info">
+                    <div class="cv_data">
+                        <div class="year">2006 - 2010</div>
+                        <div class="content">
+                            <p>Title</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                 Sit ipsam laudantium suscipit asperiores dicta, a quae ipsa.
+                                  Eaque sed omnis deserunt labore ipsum! Quod, labore dicta</p>
+                        </div>
+                    </div>
+                    <div class="cv_data">
+                        <div class="year">2006 - 2010</div>
+                        <div class="content">
+                            <p>Title</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                 Sit ipsam laudantium suscipit asperiores dicta, a quae ipsa.
+                                  Eaque sed omnis deserunt labore ipsum! Quod, labore dicta</p>
+                        </div>
+                    </div>
+                    <div class="cv_data">
+                        <div class="year">2006 - 2010</div>
+                        <div class="content">
+                            <p>Title</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                 Sit ipsam laudantium suscipit asperiores dicta, a quae ipsa.
+                                  Eaque sed omnis deserunt labore ipsum! Quod, labore dicta</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="cv_item cv_experience">
+                <div class="cv_title">Experience</div>
+                <div class="cv_info">
+                    <div class="cv_data">
+                        <div class="year">2006 - 2010</div>
+                        <div class="content">
+                            <p>Title</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                 Sit ipsam laudantium suscipit asperiores dicta, a quae ipsa.
+                                  Eaque sed omnis deserunt labore ipsum! Quod, labore dicta</p>
+                        </div>
+                    </div>
+                    <div class="cv_data">
+                        <div class="year">2006 - 2010</div>
+                        <div class="content">
+                            <p>Title</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                 Sit ipsam laudantium suscipit asperiores dicta, a quae ipsa.
+                                  Eaque sed omnis deserunt labore ipsum! Quod, labore dicta</p>
+                        </div>
+                    </div>
+                    <div class="cv_data">
+                        <div class="year">2006 - 2010</div>
+                        <div class="content">
+                            <p>Title</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                 Sit ipsam laudantium suscipit asperiores dicta, a quae ipsa.
+                                  Eaque sed omnis deserunt labore ipsum! Quod, labore dicta</p>
+                        </div>
+                    </div>
+                    <div class="cv_data">
+                        <div class="year">2006 - 2010</div>
+                        <div class="content">
+                            <p>Title</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                 Sit ipsam laudantium suscipit asperiores dicta, a quae ipsa.
+                                  Eaque sed omnis deserunt labore ipsum! Quod, labore dicta</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="cv_item cv_interests">
+                <div class="cv_title">Interests</div>
+                <div class="cv_info">
+                    <div class="interests">
+                        <div class="int_icon">
+                        <i class="fa-solid fa-volleyball"></i>
+                        </div>
+                        <div class="int_data">
+                           volleyball
+                        </div>
+                    </div>
+                    <div class="interests">
+                        <div class="int_icon">
+                        <i class="fa-solid fa-book-open"></i>
+                        </div>
+                        <div class="int_data">
+                            Reading
+                        </div>
+                    </div>
+                    <div class="interests">
+                        <div class="int_icon">
+                        <i class="fa-solid fa-film"></i>
+                        </div>
+                        <div class="int_data">
+                           Movies
+                        </div>
+                    </div>
+                    <div class="interests">
+                        <div class="int_icon">
+                        <i class="fa-solid fa-bicycle"></i>
+                        </div>
+                        <div class="int_data">
+                           Riding
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </div>
+    </div>
+</body>
+</html>
